f934ffec5f4049533b643e16e95155954759a39d
Fred Wahab <fwahab99@gmail.com>
2021-10-17 14:29:52 -0400

    Add files via upload

---
+++ Basic List View Control/Basic List View Control/src/TopFiveDestinationList.java
@@ -0,0 +1,150 @@
+//3-3 Assignment: Developing Basic ListView Control
+//Fred Wahab
+//CS-250
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+public class TopFiveDestinationList {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+            	TopDestinationListFrame topDestinationListFrame = new TopDestinationListFrame();
+                topDestinationListFrame.setTitle("Top 5 Destination List");
+                topDestinationListFrame.setVisible(true);
+            }
+        });
+    }
+}
+
+class TopDestinationListFrame extends JFrame {
+    private DefaultListModel listModel;
+
+    public TopDestinationListFrame() {
+        super("Top Five Destination List");
+        
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        //changed size
+        setSize(900, 900);
+        //set background color.
+        getContentPane().setBackground(Color.BLUE);
+        
+
+        listModel = new DefaultListModel();
+
+        //Make updates to your top 5 list below. Import the new image files to resources directory.
+        // Updated destinations to include name, short description, and picture.
+        addDestinationNameAndPicture("1. Rio De Janeiro, Brazil (A bustling city with lots to do and see.)", new ImageIcon(getClass().getResource("/resources/rio_de_janeiro_brazil.jpg")));
+        addDestinationNameAndPicture("2. Paris, France (City of romance, more attractions than anyone can see in one holiday.)", new ImageIcon(getClass().getResource("/resources/paris_france.jpg")));
+        addDestinationNameAndPicture("3. Machu Picchu, Peru (The local people are proud to share their heritage with visitors.)", new ImageIcon(getClass().getResource("/resources/machu_picchu_peru.jpg")));
+        addDestinationNameAndPicture("4. Sydney Australia (Sydney Opera House, Manly Beach and the Blue Mountains.)", new ImageIcon(getClass().getResource("/resources/sydney_australia.jpg")));
+        addDestinationNameAndPicture("5. Cape Town, South Africa (Go to the top of Table Mountain and see spectacular views.)", new ImageIcon(getClass().getResource("/resources/cape_town_south_africa.jpg")));
+        
+        JList list = new JList(listModel);
+        JScrollPane scrollPane = new JScrollPane(list);
+
+        TextAndIconListCellRenderer renderer = new TextAndIconListCellRenderer(2);
+
+        list.setCellRenderer(renderer);
+
+        getContentPane().add(scrollPane, BorderLayout.CENTER);
+    }
+
+    private void addDestinationNameAndPicture(String text, Icon icon) {
+        TextAndIcon tai = new TextAndIcon(text, icon);
+        listModel.addElement(tai);
+    }
+}
+
+
+class TextAndIcon {
+    private String text;
+    private Icon icon;
+
+    public TextAndIcon(String text, Icon icon) {
+        this.text = text;
+        this.icon = icon;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Icon getIcon() {
+        return icon;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setIcon(Icon icon) {
+        this.icon = icon;
+    }
+}
+
+
+class TextAndIconListCellRenderer extends JLabel implements ListCellRenderer {
+    private static final Border NO_FOCUS_BORDER = new EmptyBorder(1, 1, 1, 1);
+
+    private Border insideBorder;
+
+    public TextAndIconListCellRenderer() {
+        this(0, 0, 0, 0);
+    }
+
+    public TextAndIconListCellRenderer(int padding) {
+        this(padding, padding, padding, padding);
+    }
+
+    public TextAndIconListCellRenderer(int topPadding, int rightPadding, int bottomPadding, int leftPadding) {
+        insideBorder = BorderFactory.createEmptyBorder(topPadding, leftPadding, bottomPadding, rightPadding);
+        setOpaque(true);
+    }
+
+    public Component getListCellRendererComponent(JList list, Object value,
+    int index, boolean isSelected, boolean hasFocus) {
+        // The object from the combo box model MUST be a TextAndIcon.
+        TextAndIcon tai = (TextAndIcon) value;
+
+        // Sets text and icon on 'this' JLabel.
+        setText(tai.getText());
+        setIcon(tai.getIcon());
+
+        if (isSelected) {
+            setBackground(list.getSelectionBackground());
+            setForeground(list.getSelectionForeground());
+        } else {
+            setBackground(list.getBackground());
+            setForeground(list.getForeground());
+        }
+
+        Border outsideBorder;
+
+        if (hasFocus) {
+            outsideBorder = UIManager.getBorder("List.focusCellHighlightBorder");
+        } else {
+            outsideBorder = NO_FOCUS_BORDER;
+        }
+
+        setBorder(BorderFactory.createCompoundBorder(outsideBorder, insideBorder));
+        setComponentOrientation(list.getComponentOrientation());
+        setEnabled(list.isEnabled());
+        setFont(list.getFont());
+
+        return this;
+    }
+
+    // The following methods are overridden to be empty for performance
+    // reasons. If you want to understand better why, please read:
+    //
+    // http://java.sun.com/javase/6/docs/api/javax/swing/DefaultListCellRenderer.html#override
+
+    public void validate() {}
+    public void invalidate() {}
+    public void repaint() {}
+    public void revalidate() {}
+    public void repaint(long tm, int x, int y, int width, int height) {}
+    public void repaint(Rectangle r) {}
+}
